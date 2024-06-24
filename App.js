e3c75e9721ad6ed2cfdda0e8def25544724883ac
dekDika13 <kadekdikaramasuryadi@gmail.com>
2024-06-24 13:23:15 +0800

    init search feature

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react'
+// Import axios, BASE_URL, dan API_KEY
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './src/constant'
 import { View, StyleSheet } from 'react-native'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
 
 const App = () => {
-  return (
+  const searchWeather = (location) => {
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  return(
     <View style={styles.container}>
-      <WeatherSearch />
+      <WeatherSearch searchWeather={searchWeather} />
       <WeatherInfo />
     </View>
   )
